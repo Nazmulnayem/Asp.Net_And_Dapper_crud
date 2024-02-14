b6997485865eb4d7ad1a4c29267cef2fd9943e7f
nazmulnayem <mdnazmulhasancse072@gmail.com>
2024-02-14 10:20:50 +0600

    dapper

---
+++ DapperMvcDEMO/DapperMvcDEMOUI/Controllers/PersonController.cs
@@ -0,0 +1,91 @@
+﻿using DapperMvcDemo.Data.Models.Domain;
+using DapperMvcDemo.Data.Repsitory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperMvcDEMOUI.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository _personRepo;
+        public PersonController(IPersonRepository personRepo)
+        {
+            _personRepo = personRepo;
+        }
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Person person)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                    return View(person);
+                bool addPersonRESULT = await _personRepo.AddAsync(person);
+                if(addPersonRESULT) {
+                    TempData["msg"] = "Successfully added";
+                }
+                else
+                {
+                    TempData["msg"] = "Could not added";
+                }
+               
+            }
+            catch (Exception ex)
+            {
+
+                TempData["msg"] = "Could not added";
+            }
+
+            return RedirectToAction(nameof(Add));
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            
+            var person = await _personRepo.GetByIdAsync(id);
+            return View(person);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person person)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(person);
+                 var updateResult = await _personRepo.UpdateAsync(person);
+
+                if (updateResult)
+                {
+                    TempData["msg"] = "Successfully Updated";
+                  
+                }
+                else
+                {
+                    TempData["msg"] = "Could not Updated";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View(person);
+        }
+   
+      
+
+        public async Task<IActionResult> Getall()
+        {
+            var people = await _personRepo.GetAllAsync();
+            return View(people);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+             var deleteResult = await _personRepo.DeleteAsync(id);
+            return RedirectToAction(nameof(Getall));
+        }
+    }
+}
---
+++ DapperMvcDEMO/DapperMvcDemo.Data/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DapperMvcDemo.Data.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> GetData<T, P>(string spName, P parameters,
+            string connectionId = "conn")
+        {
+            using IDbConnection connection = new SqlConnection
+                (_config.GetConnectionString(connectionId));
+            return await connection.QueryAsync<T>
+                (spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string spName, T parameters,
+            string connectionId = "conn")
+        {
+            using IDbConnection connection = new SqlConnection
+                (_config.GetConnectionString(connectionId));
+             await connection.ExecuteAsync
+                (spName, parameters, commandType: 
+                CommandType.StoredProcedure);
+        }
+    }
+}
